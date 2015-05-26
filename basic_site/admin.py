bdc28a117a06c81b99dc122973271bf67bb4560a
Caroline Simpson <csimpson@cigionline.org>
2015-05-26 16:17:24 -0400

    Adding models to the django admin.

---
+++ basic_site/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.BasicPage)
+admin.site.register(models.BasicPageListing)
+admin.site.register(models.BasicBlock)
+admin.site.register(models.BasicBlockPlacement)
